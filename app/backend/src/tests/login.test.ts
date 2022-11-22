4aa23462c9804928b97cd23a897e18bd390f93cb
Ronan Salvador <ronansalvador@yahoo.com.br>
2022-11-22 17:34:04 -0300

    feat(test): create test login

---
+++ app/backend/src/tests/login.test.ts
@@ -0,0 +1,49 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import User from '../database/models/UserModel';
+
+import { Response } from 'superagent';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Verifica rota de Login', () => {
+  let chaiHttpResponse: Response;
+
+  before(async () => {
+    sinon
+      .stub(User, "findOne")
+      .resolves({ } as User);
+  });
+
+  after(()=>{
+    (User.findOne as sinon.SinonStub).restore();
+  })
+
+  describe('Validação de User', () => {
+
+    before(async () => {
+      chaiHttpResponse = await chai
+      .request(app)
+      .post('/login')
+      .send({
+       email: 'user@user.com',
+       password: 'secret_password',
+     });
+    });
+
+    it('Retorna status code 200', async () => {
+      expect(chaiHttpResponse).to.have.status(200);
+    });
+
+    it('Retorno um token', async () => {  
+      expect(chaiHttpResponse.body).to.have.property('token');
+    });
+  })
+
+});
