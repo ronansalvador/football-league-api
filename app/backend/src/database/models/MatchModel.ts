5865e2b217965b29a23a59e24d3c95eaa88a71e5
Ronan Salvador <ronansalvador@yahoo.com.br>
2022-11-22 16:39:19 -0300

    feat(matches): create migration and model matches, req 18

---
+++ app/backend/src/database/models/MatchModel.ts
@@ -0,0 +1,52 @@
+import { INTEGER, BOOLEAN, Model } from 'sequelize';
+import db from '.';
+import Team from './TeamModel';
+
+class Match extends Model {
+  id!: number;
+  homeTeam!: number;
+  awayTeam!: number;
+  homeTeamGoals!: number;
+  awayTeamGoals!: number;
+  inProgress!: number;
+}
+
+Match.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  homeTeam: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeam: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  homeTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: BOOLEAN,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'matches',
+  tableName: 'matches',
+  underscored: true,
+  timestamps: false,
+});
+
+Match.belongsTo(Team, { foreignKey: 'homeTeam', as: 'teamHome' });
+Match.belongsTo(Team, { foreignKey: 'awayTeam', as: 'teamAway' });
+
+export default Match;
