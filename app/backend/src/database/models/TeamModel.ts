a175038db6f2ebb122611eafc65f8e37d2959f9e
Ronan Salvador <ronansalvador@yahoo.com.br>
2022-11-22 16:19:32 -0300

    feat(teams): create migration and model teams, req 14

---
+++ app/backend/src/database/models/TeamModel.ts
@@ -0,0 +1,26 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+
+class Team extends Model {
+  id!: number;
+  teamName!: string;
+}
+
+Team.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: INTEGER,
+  },
+  teamName: {
+    allowNull: false,
+    type: STRING,
+  },
+}, {
+  sequelize: db,
+  timestamps: false,
+  underscored: true,
+});
+
+export default Team;
